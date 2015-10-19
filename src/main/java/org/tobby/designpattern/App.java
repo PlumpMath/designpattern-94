df82abdd0d9ebb6e95de206144dfc4df854a65ac
Linhoo Li <linhu.li2012@hotmail.com>
2015-10-19 19:56:10 +0800

    First Commit

---
+++ src/main/java/org/tobby/designpattern/App.java
@@ -0,0 +1,13 @@
+package org.tobby.designpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
