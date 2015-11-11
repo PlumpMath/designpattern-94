c2072595ffd9b2570cb22768a9e489a8cc2c6cb4
Li Linhu <linhu.li2012@hotmail.com>
2015-11-11 17:30:40 +0800

    Add the MenuComponent and MenuItem class for composite pattern

---
+++ src/main/java/com/tobby/designpattern/composite/MenuItem.java
@@ -0,0 +1,45 @@
+package com.tobby.designpattern.composite;
+
+public class MenuItem extends MenuComponent {
+	String name;
+	String description;
+	boolean vegetarian;
+	double price;
+
+	public MenuItem(String name, String description, boolean vegetarian, double price) {
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegetarian;
+		this.price = price;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	@Override
+	public double getPrice() {
+		return price;
+	}
+
+	@Override
+	public void print() {
+		System.out.print("  " + name);
+		if (vegetarian) {
+			System.out.print("(v)");
+		}
+		System.out.println(", " + description + "   --- " + price);
+	}
+
+}
